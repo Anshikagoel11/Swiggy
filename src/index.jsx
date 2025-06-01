46469a70e4c9e1d7e68ee063a2904d9495c3b358
AnshikaGoel <11402anshikagoyal@gmail.com>
2025-06-01 13:44:10 +0530

    a

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css"; 
+import Headers from "./components/headers";
+
 
 const App = () => {
   return (
-    <div className="bg-yellow-400 text-black p-4 m-4 rounded font-bold">
-      Tailwind is working!
-    </div>
+   <Headers/>
   );
 };
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App />);
